78532396264274ec815ab4f20f20b4aae6eefe7f
Ibrahim-ALkhder <alkhder09@gmail.com>
2026-06-13 01:52:30 +0300

    Update api.js

--- src/data/api.js
+++ src/data/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3001/api";
+const API_BASE = "https://ibrahim-portfolio-api.onrender.com";
 
 function getAuthHeaders() {
   const token = localStorage.getItem("adminToken");
